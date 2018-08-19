621e5a1f9016386901b5588a273966842bd625e8
Takashi Kokubun <takashikkbn@gmail.com>
2018-08-19 15:31:48 +0900

    Prepare code to run DDL

--- main.go
+++ main.go
@@ -1,15 +1,51 @@
 package main
 
 import (
+	"database/sql"
+	"log"
+
 	"github.com/go-sql-driver/mysql"
 )
 
-func buildMysqlConfig() *mysql.Config {
+func buildMysqlDSN() string {
 	config := mysql.NewConfig()
-	return config
+	config.User = "root"
+	config.Passwd = ""
+	config.Net = "tcp"
+	config.Addr = "127.0.0.1:3306"
+	config.DBName = "test"
+	return config.FormatDSN()
 }
 
 func main() {
-	println("Usage: schemasql [sql file]")
-	buildMysqlConfig()
+	dsn := buildMysqlDSN()
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	transaction, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sql := `
+		CREATE TABLE user (
+		  id BIGINT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+		  name VARCHAR(191) UNIQUE,
+		  salt VARCHAR(20),
+		  password VARCHAR(40),
+		  display_name TEXT,
+		  avatar_icon TEXT,
+		  created_at DATETIME NOT NULL
+		) Engine=InnoDB DEFAULT CHARSET=utf8mb4;
+	`
+
+	if _, err := transaction.Exec(sql); err != nil {
+		transaction.Rollback()
+		log.Fatal(err)
+	}
+	transaction.Commit()
+	println("success!")
 }
