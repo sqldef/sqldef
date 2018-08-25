1e6185b0c7122f4c76a600115ddd2d10eb52be40
Takashi Kokubun <takashikkbn@gmail.com>
2018-08-26 00:23:31 +0900

    Initial PostgreSQL integration test

--- cmd/psqldef/psqldef_test.go
+++ cmd/psqldef/psqldef_test.go
@@ -1,15 +1,22 @@
 // Integration test of psqldef command.
+//
+// Test requirement:
+//   - go command
+//   - `psql -Upostgres` must succeed
 package main
 
 import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 	"testing"
 )
 
 func TestPsqldefExport(t *testing.T) {
-	// TODO
+	resetTestDatabase()
+	out := assertedExecute(t, "psqldef", "-Upostgres", "psqldef_test", "--export")
+	assertEquals(t, out, "-- No table exists\n")
 }
 
 func TestPsqldefHelp(t *testing.T) {
@@ -25,15 +32,31 @@ func TestPsqldefHelp(t *testing.T) {
 }
 
 func TestMain(m *testing.M) {
+	resetTestDatabase()
 	mustExecute("go", "build")
 	status := m.Run()
 	os.Exit(status)
 }
 
 func mustExecute(command string, args ...string) {
-	_, err := execute(command, args...)
+	out, err := execute(command, args...)
 	if err != nil {
+		log.Printf("out: '%s'", out)
 		log.Fatal(err)
+	}
+}
+
+func assertedExecute(t *testing.T, command string, args ...string) string {
+	out, err := execute(command, args...)
+	if err != nil {
+		t.Errorf("failed to execute '%s %s' (out: '%s'): %s", command, strings.Join(args, " "), out, err)
+	}
+	return out
+}
+
+func assertEquals(t *testing.T, actual string, expected string) {
+	if expected != actual {
+		t.Errorf("expected '%s' but got '%s'", expected, actual)
 	}
 }
 
@@ -41,4 +64,9 @@ func execute(command string, args ...string) (string, error) {
 	cmd := exec.Command(command, args...)
 	out, err := cmd.Output()
 	return string(out), err
+}
+
+func resetTestDatabase() {
+	mustExecute("psql", "-Upostgres", "-c", "DROP DATABASE IF EXISTS psqldef_test;")
+	mustExecute("psql", "-Upostgres", "-c", "CREATE DATABASE psqldef_test;")
 }
