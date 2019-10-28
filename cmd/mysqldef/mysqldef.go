87bd97c5655333a946de41ed3018ee03f75bd19b
touyu <dev.akkey0222@gmail.com>
2019-10-28 11:53:49 +0900

    fix skip-drop option

--- cmd/mysqldef/mysqldef.go
+++ cmd/mysqldef/mysqldef.go
@@ -26,7 +26,7 @@ func parseOptions(args []string) (adapter.Config, *sqldef.Options) {
 		File     string `long:"file" description:"Read schema SQL from the file, rather than stdin" value-name:"sql_file" default:"-"`
 		DryRun   bool   `long:"dry-run" description:"Don't run DDLs but just show them"`
 		Export   bool   `long:"export" description:"Just dump the current schema to stdout"`
-		SkipDrop bool   `long:"safety" description:"Not make destructive changes such as Drop"`
+		SkipDrop bool   `long:"skip-drop" description:"Not make destructive changes such as Drop"`
 		Help     bool   `long:"help" description:"Show this help"`
 	}
 
