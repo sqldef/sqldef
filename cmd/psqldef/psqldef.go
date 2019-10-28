aefbadccb4fa66bce5bfe7dd6ee068831485bfaf
touyu <dev.akkey0222@gmail.com>
2019-10-28 11:55:34 +0900

    Add skip-drop option to psqldef

--- cmd/psqldef/psqldef.go
+++ cmd/psqldef/psqldef.go
@@ -25,6 +25,7 @@ func parseOptions(args []string) (adapter.Config, *sqldef.Options) {
 		File     string `short:"f" long:"file" description:"Read schema SQL from the file, rather than stdin" value-name:"filename" default:"-"`
 		DryRun   bool   `long:"dry-run" description:"Don't run DDLs but just show them"`
 		Export   bool   `long:"export" description:"Just dump the current schema to stdout"`
+		SkipDrop bool   `long:"skip-drop" description:"Not make destructive changes such as Drop"`
 		Help     bool   `long:"help" description:"Show this help"`
 	}
 
@@ -52,9 +53,10 @@ func parseOptions(args []string) (adapter.Config, *sqldef.Options) {
 	database := args[0]
 
 	options := sqldef.Options{
-		SqlFile: opts.File,
-		DryRun:  opts.DryRun,
-		Export:  opts.Export,
+		SqlFile:  opts.File,
+		DryRun:   opts.DryRun,
+		Export:   opts.Export,
+		SkipDrop: opts.SkipDrop,
 	}
 
 	password, ok := os.LookupEnv("PGPASS")
