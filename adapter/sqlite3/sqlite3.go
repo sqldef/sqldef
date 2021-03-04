d7469180c0d3712036ca1537dbf8748da17fe4a6
tsuty <3104@tsuty.com>
2021-03-05 03:32:06 +0900

    fix: including SQLite internal schema objects. exclude 'sqlite_' prefix

--- adapter/sqlite3/sqlite3.go
+++ adapter/sqlite3/sqlite3.go
@@ -26,7 +26,7 @@ func NewDatabase(config adapter.Config) (adapter.Database, error) {
 
 func (d *Sqlite3Database) TableNames() ([]string, error) {
 	rows, err := d.db.Query(
-		`select tbl_name from sqlite_master where type = 'table'`,
+		`select tbl_name from sqlite_master where type = 'table' and tbl_name not like 'sqlite_%'`,
 	)
 	if err != nil {
 		return nil, err
