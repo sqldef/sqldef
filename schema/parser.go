85175e418adc69414d7b79063e015960f43c3983
Takashi Kokubun <takashikkbn@gmail.com>
2024-01-04 23:47:58 -0800

    Reorder schema functions by their importance

--- schema/parser.go
+++ schema/parser.go
@@ -11,85 +11,165 @@ import (
 	"github.com/sqldef/sqldef/parser"
 )
 
-// Convert back `type BoolVal bool`
-func castBool(val parser.BoolVal) bool {
-	ret, _ := strconv.ParseBool(fmt.Sprint(val))
-	return ret
-}
-
-func castBoolPtr(val *parser.BoolVal) *bool {
-	if val == nil {
-		return nil
-	}
-	ret := castBool(*val)
-	return &ret
-}
-
-func parseValue(val *parser.SQLVal) *Value {
-	if val == nil {
-		return nil
-	}
-
-	var valueType ValueType
-	if val.Type == parser.StrVal {
-		valueType = ValueTypeStr
-	} else if val.Type == parser.IntVal {
-		valueType = ValueTypeInt
-	} else if val.Type == parser.FloatVal {
-		valueType = ValueTypeFloat
-	} else if val.Type == parser.HexNum {
-		valueType = ValueTypeHexNum
-	} else if val.Type == parser.HexVal {
-		valueType = ValueTypeHex
-	} else if val.Type == parser.ValArg {
-		valueType = ValueTypeValArg
-	} else if val.Type == parser.BitVal {
-		valueType = ValueTypeBit
-	} else if val.Type == parser.ValBool {
-		valueType = ValueTypeBool
-	} else {
-		return nil // TODO: Unreachable, but handle this properly...
-	}
-
-	ret := Value{
-		valueType: valueType,
-		raw:       val.Val,
-	}
-
-	switch valueType {
-	case ValueTypeStr, ValueTypeBool:
-		ret.strVal = string(val.Val)
-	case ValueTypeInt:
-		intVal, _ := strconv.Atoi(string(val.Val)) // TODO: handle error
-		ret.intVal = intVal
-	case ValueTypeFloat:
-		floatVal, _ := strconv.ParseFloat(string(val.Val), 64) // TODO: handle error
-		ret.floatVal = floatVal
-	case ValueTypeBit:
-		if string(val.Val) == "1" {
-			ret.bitVal = true
-		} else {
-			ret.bitVal = false
-		}
-	}
-
-	return &ret
-}
-
-// Assume an integer length. Maybe useful only for index lengths.
-// TODO: Change IndexColumn.Length in parser.y to integer in the first place
-func parseLength(val *parser.SQLVal) (*int, error) {
-	if val == nil {
-		return nil, nil
-	}
-	if val.Type != parser.IntVal {
-		return nil, fmt.Errorf("Expected a length to be int, but got ValType: %d (%#v)", val.Type, val.Val)
-	}
-	intVal, err := strconv.Atoi(string(val.Val)) // TODO: handle error
+// Parse `ddls`, which is expected to `;`-concatenated DDLs
+// and not to include destructive DDL.
+func ParseDDLs(mode GeneratorMode, sqlParser database.Parser, sql string, defaultSchema string) ([]DDL, error) {
+	ddls, err := sqlParser.Parse(sql)
 	if err != nil {
 		return nil, err
 	}
-	return &intVal, nil
+
+	var result []DDL
+	for _, ddl := range ddls {
+		parsed, err := parseDDL(mode, ddl.DDL, ddl.Statement, defaultSchema)
+		if err != nil {
+			return result, err
+		}
+		result = append(result, parsed)
+	}
+	return result, nil
+}
+
+// Parse DDL like `CREATE TABLE` or `ALTER TABLE`.
+// This doesn't support destructive DDL like `DROP TABLE`.
+func parseDDL(mode GeneratorMode, ddl string, stmt parser.Statement, defaultSchema string) (DDL, error) {
+	switch stmt := stmt.(type) {
+	case *parser.DDL:
+		if stmt.Action == parser.CreateStr {
+			// TODO: handle other create DDL as error?
+			table, err := parseTable(mode, stmt, defaultSchema)
+			if err != nil {
+				return nil, err
+			}
+			return &CreateTable{
+				statement: ddl,
+				table:     table,
+			}, nil
+		} else if stmt.Action == parser.CreateIndexStr {
+			index, err := parseIndex(stmt)
+			if err != nil {
+				return nil, err
+			}
+			return &CreateIndex{
+				statement: ddl,
+				tableName: normalizedTableName(mode, stmt.Table, defaultSchema),
+				index:     index,
+			}, nil
+		} else if stmt.Action == parser.AddIndexStr {
+			index, err := parseIndex(stmt)
+			if err != nil {
+				return nil, err
+			}
+			return &AddIndex{
+				statement: ddl,
+				tableName: normalizedTableName(mode, stmt.Table, defaultSchema),
+				index:     index,
+			}, nil
+		} else if stmt.Action == parser.AddPrimaryKeyStr {
+			index, err := parseIndex(stmt)
+			if err != nil {
+				return nil, err
+			}
+			return &AddPrimaryKey{
+				statement: ddl,
+				tableName: normalizedTableName(mode, stmt.Table, defaultSchema),
+				index:     index,
+			}, nil
+		} else if stmt.Action == parser.AddForeignKeyStr {
+			indexColumns := []string{}
+			for _, indexColumn := range stmt.ForeignKey.IndexColumns {
+				indexColumns = append(indexColumns, indexColumn.String())
+			}
+			referenceColumns := []string{}
+			for _, referenceColumn := range stmt.ForeignKey.ReferenceColumns {
+				referenceColumns = append(referenceColumns, referenceColumn.String())
+			}
+
+			return &AddForeignKey{
+				statement: ddl,
+				tableName: normalizedTableName(mode, stmt.Table, defaultSchema),
+				foreignKey: ForeignKey{
+					constraintName:    stmt.ForeignKey.ConstraintName.String(),
+					indexName:         stmt.ForeignKey.IndexName.String(),
+					indexColumns:      indexColumns,
+					referenceName:     normalizedTableName(mode, stmt.ForeignKey.ReferenceName, defaultSchema),
+					referenceColumns:  referenceColumns,
+					onDelete:          stmt.ForeignKey.OnDelete.String(),
+					onUpdate:          stmt.ForeignKey.OnUpdate.String(),
+					notForReplication: stmt.ForeignKey.NotForReplication,
+				},
+			}, nil
+		} else if stmt.Action == parser.CreatePolicyStr {
+			scope := make([]string, len(stmt.Policy.To))
+			for i, to := range stmt.Policy.To {
+				scope[i] = to.String()
+			}
+			var using, withCheck string
+			if stmt.Policy.Using != nil {
+				using = parser.String(stmt.Policy.Using.Expr)
+			}
+			if stmt.Policy.WithCheck != nil {
+				withCheck = parser.String(stmt.Policy.WithCheck.Expr)
+			}
+			return &AddPolicy{
+				statement: ddl,
+				tableName: normalizedTableName(mode, stmt.Table, defaultSchema),
+				policy: Policy{
+					name:       stmt.Policy.Name.String(),
+					permissive: string(stmt.Policy.Permissive),
+					scope:      string(stmt.Policy.Scope),
+					roles:      scope,
+					using:      using,
+					withCheck:  withCheck,
+				},
+			}, nil
+		} else if stmt.Action == parser.CreateViewStr {
+			return &View{
+				statement:    ddl,
+				viewType:     strings.ToUpper(strings.TrimPrefix(stmt.View.Action, "create ")),
+				securityType: strings.ToUpper(stmt.View.SecurityType),
+				name:         normalizedTableName(mode, stmt.View.Name, defaultSchema),
+				definition:   parser.String(stmt.View.Definition),
+			}, nil
+		} else if stmt.Action == parser.CreateTriggerStr {
+			body := []string{}
+			for _, triggerStatement := range stmt.Trigger.Body {
+				body = append(body, parser.String(triggerStatement))
+			}
+
+			return &Trigger{
+				statement: ddl,
+				name:      stmt.Trigger.Name.String(),
+				tableName: stmt.Trigger.TableName.Name,
+				time:      stmt.Trigger.Time,
+				event:     stmt.Trigger.Event,
+				body:      body,
+			}, nil
+		} else if stmt.Action == parser.CreateTypeStr {
+			return &Type{
+				name:       normalizedTableName(mode, stmt.Type.Name, defaultSchema),
+				statement:  ddl,
+				enumValues: stmt.Type.Type.EnumValues,
+			}, nil
+		} else if stmt.Action == parser.CommentStr {
+			return &Comment{
+				statement: ddl,
+				comment:   *stmt.Comment,
+			}, nil
+		} else if stmt.Action == parser.CreateExtensionStr {
+			return &Extension{
+				statement: ddl,
+				extension: *stmt.Extension,
+			}, nil
+		} else {
+			return nil, fmt.Errorf(
+				"unsupported type of DDL action '%s': %s",
+				stmt.Action, ddl,
+			)
+		}
+	default:
+		return nil, fmt.Errorf("unsupported type of SQL (only DDL is supported): %s", ddl)
+	}
 }
 
 func parseTable(mode GeneratorMode, stmt *parser.DDL, defaultSchema string) (Table, error) {
@@ -316,197 +396,71 @@ func parseIndex(stmt *parser.DDL) (Index, error) {
 	}, nil
 }
 
-// Parse DDL like `CREATE TABLE` or `ALTER TABLE`.
-// This doesn't support destructive DDL like `DROP TABLE`.
-func parseDDL(mode GeneratorMode, ddl string, stmt parser.Statement, defaultSchema string) (DDL, error) {
-	switch stmt := stmt.(type) {
-	case *parser.DDL:
-		if stmt.Action == parser.CreateStr {
-			// TODO: handle other create DDL as error?
-			table, err := parseTable(mode, stmt, defaultSchema)
-			if err != nil {
-				return nil, err
-			}
-			return &CreateTable{
-				statement: ddl,
-				table:     table,
-			}, nil
-		} else if stmt.Action == parser.CreateIndexStr {
-			index, err := parseIndex(stmt)
-			if err != nil {
-				return nil, err
-			}
-			return &CreateIndex{
-				statement: ddl,
-				tableName: normalizedTableName(mode, stmt.Table, defaultSchema),
-				index:     index,
-			}, nil
-		} else if stmt.Action == parser.AddIndexStr {
-			index, err := parseIndex(stmt)
-			if err != nil {
-				return nil, err
-			}
-			return &AddIndex{
-				statement: ddl,
-				tableName: normalizedTableName(mode, stmt.Table, defaultSchema),
-				index:     index,
-			}, nil
-		} else if stmt.Action == parser.AddPrimaryKeyStr {
-			index, err := parseIndex(stmt)
-			if err != nil {
-				return nil, err
-			}
-			return &AddPrimaryKey{
-				statement: ddl,
-				tableName: normalizedTableName(mode, stmt.Table, defaultSchema),
-				index:     index,
-			}, nil
-		} else if stmt.Action == parser.AddForeignKeyStr {
-			indexColumns := []string{}
-			for _, indexColumn := range stmt.ForeignKey.IndexColumns {
-				indexColumns = append(indexColumns, indexColumn.String())
-			}
-			referenceColumns := []string{}
-			for _, referenceColumn := range stmt.ForeignKey.ReferenceColumns {
-				referenceColumns = append(referenceColumns, referenceColumn.String())
-			}
-
-			return &AddForeignKey{
-				statement: ddl,
-				tableName: normalizedTableName(mode, stmt.Table, defaultSchema),
-				foreignKey: ForeignKey{
-					constraintName:    stmt.ForeignKey.ConstraintName.String(),
-					indexName:         stmt.ForeignKey.IndexName.String(),
-					indexColumns:      indexColumns,
-					referenceName:     normalizedTableName(mode, stmt.ForeignKey.ReferenceName, defaultSchema),
-					referenceColumns:  referenceColumns,
-					onDelete:          stmt.ForeignKey.OnDelete.String(),
-					onUpdate:          stmt.ForeignKey.OnUpdate.String(),
-					notForReplication: stmt.ForeignKey.NotForReplication,
-				},
-			}, nil
-		} else if stmt.Action == parser.CreatePolicyStr {
-			scope := make([]string, len(stmt.Policy.To))
-			for i, to := range stmt.Policy.To {
-				scope[i] = to.String()
-			}
-			var using, withCheck string
-			if stmt.Policy.Using != nil {
-				using = parser.String(stmt.Policy.Using.Expr)
-			}
-			if stmt.Policy.WithCheck != nil {
-				withCheck = parser.String(stmt.Policy.WithCheck.Expr)
-			}
-			return &AddPolicy{
-				statement: ddl,
-				tableName: normalizedTableName(mode, stmt.Table, defaultSchema),
-				policy: Policy{
-					name:       stmt.Policy.Name.String(),
-					permissive: string(stmt.Policy.Permissive),
-					scope:      string(stmt.Policy.Scope),
-					roles:      scope,
-					using:      using,
-					withCheck:  withCheck,
-				},
-			}, nil
-		} else if stmt.Action == parser.CreateViewStr {
-			return &View{
-				statement:    ddl,
-				viewType:     strings.ToUpper(strings.TrimPrefix(stmt.View.Action, "create ")),
-				securityType: strings.ToUpper(stmt.View.SecurityType),
-				name:         normalizedTableName(mode, stmt.View.Name, defaultSchema),
-				definition:   parser.String(stmt.View.Definition),
-			}, nil
-		} else if stmt.Action == parser.CreateTriggerStr {
-			body := []string{}
-			for _, triggerStatement := range stmt.Trigger.Body {
-				body = append(body, parser.String(triggerStatement))
-			}
-
-			return &Trigger{
-				statement: ddl,
-				name:      stmt.Trigger.Name.String(),
-				tableName: stmt.Trigger.TableName.Name,
-				time:      stmt.Trigger.Time,
-				event:     stmt.Trigger.Event,
-				body:      body,
-			}, nil
-		} else if stmt.Action == parser.CreateTypeStr {
-			return &Type{
-				name:       normalizedTableName(mode, stmt.Type.Name, defaultSchema),
-				statement:  ddl,
-				enumValues: stmt.Type.Type.EnumValues,
-			}, nil
-		} else if stmt.Action == parser.CommentStr {
-			return &Comment{
-				statement: ddl,
-				comment:   *stmt.Comment,
-			}, nil
-		} else if stmt.Action == parser.CreateExtensionStr {
-			return &Extension{
-				statement: ddl,
-				extension: *stmt.Extension,
-			}, nil
-		} else {
-			return nil, fmt.Errorf(
-				"unsupported type of DDL action '%s': %s",
-				stmt.Action, ddl,
-			)
-		}
-	default:
-		return nil, fmt.Errorf("unsupported type of SQL (only DDL is supported): %s", ddl)
+func parseValue(val *parser.SQLVal) *Value {
+	if val == nil {
+		return nil
 	}
+
+	var valueType ValueType
+	if val.Type == parser.StrVal {
+		valueType = ValueTypeStr
+	} else if val.Type == parser.IntVal {
+		valueType = ValueTypeInt
+	} else if val.Type == parser.FloatVal {
+		valueType = ValueTypeFloat
+	} else if val.Type == parser.HexNum {
+		valueType = ValueTypeHexNum
+	} else if val.Type == parser.HexVal {
+		valueType = ValueTypeHex
+	} else if val.Type == parser.ValArg {
+		valueType = ValueTypeValArg
+	} else if val.Type == parser.BitVal {
+		valueType = ValueTypeBit
+	} else if val.Type == parser.ValBool {
+		valueType = ValueTypeBool
+	} else {
+		return nil // TODO: Unreachable, but handle this properly...
+	}
+
+	ret := Value{
+		valueType: valueType,
+		raw:       val.Val,
+	}
+
+	switch valueType {
+	case ValueTypeStr, ValueTypeBool:
+		ret.strVal = string(val.Val)
+	case ValueTypeInt:
+		intVal, _ := strconv.Atoi(string(val.Val)) // TODO: handle error
+		ret.intVal = intVal
+	case ValueTypeFloat:
+		floatVal, _ := strconv.ParseFloat(string(val.Val), 64) // TODO: handle error
+		ret.floatVal = floatVal
+	case ValueTypeBit:
+		if string(val.Val) == "1" {
+			ret.bitVal = true
+		} else {
+			ret.bitVal = false
+		}
+	}
+
+	return &ret
 }
 
-// Parse `ddls`, which is expected to `;`-concatenated DDLs
-// and not to include destructive DDL.
-func ParseDDLs(mode GeneratorMode, sqlParser database.Parser, sql string, defaultSchema string) ([]DDL, error) {
-	ddls, err := sqlParser.Parse(sql)
+// Assume an integer length. Maybe useful only for index lengths.
+// TODO: Change IndexColumn.Length in parser.y to integer in the first place
+func parseLength(val *parser.SQLVal) (*int, error) {
+	if val == nil {
+		return nil, nil
+	}
+	if val.Type != parser.IntVal {
+		return nil, fmt.Errorf("Expected a length to be int, but got ValType: %d (%#v)", val.Type, val.Val)
+	}
+	intVal, err := strconv.Atoi(string(val.Val)) // TODO: handle error
 	if err != nil {
 		return nil, err
 	}
-
-	var result []DDL
-	for _, ddl := range ddls {
-		parsed, err := parseDDL(mode, ddl.DDL, ddl.Statement, defaultSchema)
-		if err != nil {
-			return result, err
-		}
-		result = append(result, parsed)
-	}
-	return result, nil
-}
-
-// Replace pseudo collation "binary" with "{charset}_bin"
-func normalizeCollate(collate string, table parser.TableSpec) string {
-	if collate == "binary" {
-		return table.Options["default charset"] + "_bin"
-	} else {
-		return collate
-	}
-}
-
-// Qualify Postgres/Mssql schema
-func normalizedTableName(mode GeneratorMode, tableName parser.TableName, defaultSchema string) string {
-	table := tableName.Name
-	if mode == GeneratorModePostgres || mode == GeneratorModeMssql {
-		if len(tableName.Schema) > 0 {
-			table = tableName.Schema + "." + table
-		} else {
-			table = defaultSchema + "." + table
-		}
-	}
-	return table
-}
-
-func normalizedTable(mode GeneratorMode, tableName string, defaultSchema string) string {
-	switch mode {
-	case GeneratorModePostgres, GeneratorModeMssql:
-		schema, table := splitTableName(tableName, defaultSchema)
-		return fmt.Sprintf("%s.%s", schema, table)
-	default:
-		return tableName
-	}
+	return &intVal, nil
 }
 
 func parseIdentity(opt *parser.IdentityOpt) *Identity {
@@ -598,4 +552,50 @@ func parseGenerated(genc *parser.GeneratedColumn) *Generated {
 		expr:          parser.String(genc.Expr),
 		generatedType: typ,
 	}
+}
+
+// Qualify Postgres/Mssql schema
+func normalizedTableName(mode GeneratorMode, tableName parser.TableName, defaultSchema string) string {
+	table := tableName.Name
+	if mode == GeneratorModePostgres || mode == GeneratorModeMssql {
+		if len(tableName.Schema) > 0 {
+			table = tableName.Schema + "." + table
+		} else {
+			table = defaultSchema + "." + table
+		}
+	}
+	return table
+}
+
+func normalizedTable(mode GeneratorMode, tableName string, defaultSchema string) string {
+	switch mode {
+	case GeneratorModePostgres, GeneratorModeMssql:
+		schema, table := splitTableName(tableName, defaultSchema)
+		return fmt.Sprintf("%s.%s", schema, table)
+	default:
+		return tableName
+	}
+}
+
+// Replace pseudo collation "binary" with "{charset}_bin"
+func normalizeCollate(collate string, table parser.TableSpec) string {
+	if collate == "binary" {
+		return table.Options["default charset"] + "_bin"
+	} else {
+		return collate
+	}
+}
+
+// Convert back `type BoolVal bool`
+func castBool(val parser.BoolVal) bool {
+	ret, _ := strconv.ParseBool(fmt.Sprint(val))
+	return ret
+}
+
+func castBoolPtr(val *parser.BoolVal) *bool {
+	if val == nil {
+		return nil
+	}
+	ret := castBool(*val)
+	return &ret
 }
