8bef7ea39431bd82d61127bb1e40063da2622be0
FUJI Goro <g.psy.va@gmail.com>
2025-10-13 14:17:16 +0900

    tweaks

--- util/logutil.go
+++ util/logutil.go
@@ -9,26 +9,26 @@ import (
 // InitSlog configures slog based on LOG_LEVEL environment variable.
 // Supported levels: debug, info, warn, error
 func InitSlog() {
-	logLevel := os.Getenv("LOG_LEVEL")
-	var level slog.Level
+	if logLevel, ok := os.LookupEnv("LOG_LEVEL"); ok {
+		var level slog.Level
 
-	switch strings.ToLower(logLevel) {
-	case "debug":
-		level = slog.LevelDebug
-	case "info":
-		level = slog.LevelInfo
-	case "warn":
-		level = slog.LevelWarn
-	case "error":
-		level = slog.LevelError
-	default:
-		// Default to Info level if not set or invalid
-		return
-	}
+		switch strings.ToLower(logLevel) {
+		case "debug":
+			level = slog.LevelDebug
+		case "info":
+			level = slog.LevelInfo
+		case "warn":
+			level = slog.LevelWarn
+		case "error":
+			level = slog.LevelError
+		default:
+			return
+		}
 
-	opts := &slog.HandlerOptions{
-		Level: level,
+		opts := &slog.HandlerOptions{
+			Level: level,
+		}
+		handler := slog.NewTextHandler(os.Stderr, opts)
+		slog.SetDefault(slog.New(handler))
 	}
-	handler := slog.NewTextHandler(os.Stderr, opts)
-	slog.SetDefault(slog.New(handler))
 }
