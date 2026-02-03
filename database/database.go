62fbe48c70c380c47d30ad395d02fb36cc2c2bf6
winebarrel <sugawara@winebarrel.jp>
2026-02-03 17:41:04 +0900

    Fix line comment check: Do not skip multi-line SQL

--- database/database.go
+++ database/database.go
@@ -237,7 +237,7 @@ func RunDDLs(d Database, ddls []string, beforeApply string, ddlSuffix string, lo
 	for _, ddl := range ddlsInTx {
 		logger.Printf("%s;\n", ddl)
 
-		if strings.HasPrefix(ddl, "-- ") {
+		if strings.HasPrefix(ddl, "-- ") && !strings.Contains(strings.TrimSpace(ddl), "\n") {
 			// Skip commented DDLs (e.g., "-- Skipped: ...")
 			continue
 		}
@@ -263,7 +263,7 @@ func RunDDLs(d Database, ddls []string, beforeApply string, ddlSuffix string, lo
 	for _, ddl := range ddlsNotInTx {
 		logger.Printf("%s;\n", ddl)
 		// Skip ddlSuffix and execution for commented DDLs (e.g., "-- Skipped: ...")
-		if !strings.HasPrefix(ddl, "-- ") {
+		if !strings.HasPrefix(ddl, "-- ") && !strings.Contains(strings.TrimSpace(ddl), "\n") {
 			logger.Print(ddlSuffix)
 			_, err = d.DB().Exec(ddl)
 			if err != nil {
